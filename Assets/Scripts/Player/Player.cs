f41785235d5613bb5f53e4d2175f41092dc5e0c8
BakewellTorte <elfishone@Katherines-MacBook-Pro.local>
2016-08-20 12:22:45 -0400

    Added Players, Added Separate Controls,

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public float Size = 1;
+	public float MovementSpeedMultiplier = 1;
+
+	public string HorizontalAxis = "Horizontal";
+	public string VerticalAxis = "Vertical";
+
+	// Use this for initialization
+	private Rigidbody m_rigidBody;
+	void Start () 
+	{
+		transform.localScale = new Vector3 (Size, Size, Size);	
+
+		m_rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	private float m_prevSize;
+	void FixedUpdate () 
+	{
+		float horizontalInput = Input.GetAxis (HorizontalAxis);
+		float verticalInput = Input.GetAxis (VerticalAxis);
+
+		if (m_prevSize != Size) 
+		{
+			transform.localScale = new Vector3 (Size, Size, Size);	
+			m_prevSize = Size;
+		}
+
+		//print (horizontalInput);
+		m_rigidBody.velocity = new Vector3 (MovementSpeedMultiplier * horizontalInput, 0, MovementSpeedMultiplier * verticalInput);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		
+	}
+}
