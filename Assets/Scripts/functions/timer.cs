7b195351aea8c11b5691ac2e50d0c59d8c2d6cb5
mrformidable <MichaelAi@Michaels-MacBook-Pro-2.local>
2016-08-20 15:23:12 -0400

    timerUpdate

---
+++ Assets/Scripts/functions/timer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class timer : MonoBehaviour {
+
+	public float timeLeft = 10f;
+	public Text timerText;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		InvokeRepeating ("decreaseTimeRemaining", 0.05f, 0.5f);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (timeLeft == 0)
+		{
+		//	sendMessageUpward("timeElapsed");
+
+		}
+
+	//	GuiText.text = timeLeft + " Seconds remaining!";
+	}
+	void decreaseTimeRemaining()
+	{
+
+		if (timeLeft < 0.5f) {
+			//	sendMessageUpward("timeElapsed");
+			timeLeft = 0.00f;
+			timerText.text = "Game Over";
+
+		} else {
+
+			timeLeft -= 0.54500f;
+			//timerText.text = "Let the games begin "  + timeLeft.ToString ();
+			timerText.text = "Start " + string.Format("{0:.##}",timeLeft);
+		}
+	}
+}
+
+
+
