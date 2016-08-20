e40285b8d42d8116dd17ce8a8c056fcbb6ed5f8c
BakewellTorte <elfishone@Katherines-MacBook-Pro.local>
2016-08-20 15:22:21 -0400

    paul changes

---
+++ Assets/Scripts/EndZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class EndZoneContact : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter (Collider collider) {
+		if (collider.gameObject.tag == "Player") {
+			print ("Player has entered trigger zone");
+			//track player time (score)
+			Debug.Log(string.Format("{0} has finished with a time of {1}", collider.gameObject.transform.parent.name, "sdf"));
+
+
+
+	}
+}
+}
