a9330d233cd54f46bd9c6628e823dd268f7331fe
BakewellTorte <elfishone@Katherines-MacBook-Pro.local>
2016-08-20 15:11:28 -0400

    Added Appearance

---
+++ Assets/Scripts/Player/PlayerAppearance.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAppearance : MonoBehaviour {
+
+	public GameObject[] Images;
+
+	// Use this for initialization
+	void Start () {
+		int image = Random.Range (0, Images.Length - 1);
+		Images [image].SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
